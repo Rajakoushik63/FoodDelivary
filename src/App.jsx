8fd4f182cb55b2d045efd80fba3ad36f8161c369
rajak <rajakoushik8008@gmail.com>
2025-01-29 00:20:08 +0530

    update the index.css file and add the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Cart from "./components/Cart/Cart";
+import PlaceOrder from "./components/PlaceOrder/PlaceOrder";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
