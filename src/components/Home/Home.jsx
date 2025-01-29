68b707f491c5be62282f73971757b2bbe9b7db32
rajak <rajakoushik8008@gmail.com>
2025-01-29 15:02:24 +0530

    installed  prop-types module and updated the ExploreMenu and added it in Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,15 @@
 // import React from 'react'
+import { useState } from "react";
+import ExploreMenu from "../ExploreMenu/ExploreMenu";
 import Header from "../Header/Header";
 import "./Home.css";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 };
