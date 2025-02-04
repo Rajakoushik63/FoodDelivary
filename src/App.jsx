527a996cf85d6687e719033a64d1111bd920222c
rajak <rajakoushik8008@gmail.com>
2025-02-04 12:34:14 +0530

    update the header_img and logo and

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 import { useState } from "react";
 import Verify from "./components/Verify/Verify";
+import MyOrders from "./components/MyOrders/MyOrders";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -20,6 +21,7 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
           <Route path="/verify" element={<Verify />} />
+          <Route path="/myorders" element={<MyOrders />} />
         </Routes>
       </div>
       <Footer />
